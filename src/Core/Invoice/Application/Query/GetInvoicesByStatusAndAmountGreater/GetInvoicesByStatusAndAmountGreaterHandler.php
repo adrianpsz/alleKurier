46f679089e050af8245d0d3bd40ce097fe875e59
adrian <adrian@adrianp.pl>
2024-11-18 10:35:54 +0100

    fix command invoice get by status and amount

--- src/Core/Invoice/Application/Query/GetInvoicesByStatusAndAmountGreater/GetInvoicesByStatusAndAmountGreaterHandler.php
+++ src/Core/Invoice/Application/Query/GetInvoicesByStatusAndAmountGreater/GetInvoicesByStatusAndAmountGreaterHandler.php
@@ -19,7 +19,7 @@ class GetInvoicesByStatusAndAmountGreaterHandler
     {
         $invoices = $this->invoiceRepository->getInvoicesWithGreaterAmountAndStatus(
             $query->amount,
-            InvoiceStatus::CANCELED
+            InvoiceStatus::valueFromName($query->status)
         );
 
         return array_map(function (Invoice $invoice) {
--- src/Core/Invoice/Domain/Status/InvoiceStatus.php
+++ src/Core/Invoice/Domain/Status/InvoiceStatus.php
@@ -2,9 +2,23 @@
 
 namespace App\Core\Invoice\Domain\Status;
 
+use App\Core\Invoice\Domain\Exception\InvoiceException;
+
 enum InvoiceStatus: string
 {
     case NEW = 'new';
     case PAID = 'paid';
     case CANCELED = 'canceled';
+
+    public static function valueFromName(string $name): self
+    {
+        $name = strtoupper($name);
+        try {
+            $value = constant("self::$name");
+        } catch (\Error $e) {
+            throw new InvoiceException("Wrong invoice status: ".$name);
+        }
+
+        return $value;
+    }
 }
